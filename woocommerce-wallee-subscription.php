df6d8d0d7d404c6ccb3cf9cb23a72f15eee49593
Theodor Spring <theodor.spring@wallee.com>
2021-11-22 14:34:08 +0100

    Release 1.0.10

--- woocommerce-wallee-subscription.php
+++ woocommerce-wallee-subscription.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce wallee Subscription
  * Plugin URI: https://wordpress.org/plugins/woo-wallee-subscription
  * Description: Addon to processs WooCommerce Subscriptions with wallee
- * Version: 1.0.9
+ * Version: 1.0.10
  * License: Apache2
  * License URI: http://www.apache.org/licenses/LICENSE-2.0
  * Author: customweb GmbH
@@ -36,7 +36,7 @@ if (! class_exists('WooCommerce_Wallee_Subscription')) {
          *
          * @var string
          */
-        private $version = '1.0.9';
+        private $version = '1.0.10';
 
         /**
          * The single instance of the class.
